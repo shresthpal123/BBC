bfa4e26c85dac257240a0b056485de612f08c6ea
Shresth Pal <shresthpal@gmail.com>
2019-02-06 17:06:50 +0530

    changed chrome path

--- First_GIT_Project/test_KeywordEngine.py
+++ First_GIT_Project/test_KeywordEngine.py
@@ -3,7 +3,7 @@ import test_OpenBrowser,test_NavigateURL
 import logging
 
 # chrome_path ="/Users/Shared/Jenkins/Home/jobs/Python/chromedriver 4"
-chrome_path = "chromedriver 4"
+chrome_path = "/Users/Shared/Jenkins/Home/workspace/Branch1/First_GIT_Project/chromedriver 4"
 
 
 def test_openBrowser():
